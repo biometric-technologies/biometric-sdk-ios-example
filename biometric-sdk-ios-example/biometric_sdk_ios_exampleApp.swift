c68e8df5884c47a826f530d025bd6d1e3efec7f7
Slava <slava.gornostal@gmail.com>
2023-02-05 14:27:06 +0200

    Initial Commit

---
+++ biometric-sdk-ios-example/biometric_sdk_ios_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  biometric_sdk_ios_exampleApp.swift
+//  biometric-sdk-ios-example
+//
+//  Created by Slava on 05.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct biometric_sdk_ios_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
